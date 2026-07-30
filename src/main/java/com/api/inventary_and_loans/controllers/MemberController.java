0facc071b13b5b6a6228e94af9a02a0ed0a5cd70
Daniel Fernández roldan <danielfernandezroldan2005@gmail.com>
2026-07-30 16:55:20 +0200

    Added a controller for Member model.

---
+++ src/main/java/com/api/inventary_and_loans/controllers/MemberController.java
@@ -0,0 +1,36 @@
+package com.api.inventary_and_loans.controllers;
+
+import com.api.inventary_and_loans.entities.Member;
+import com.api.inventary_and_loans.services.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping
+    public List<Member> getAllMembers() {
+        return memberService.getAllMembers();
+    }
+
+    @GetMapping("/dni/{nationalId}")
+    public ResponseEntity<Member> getMemberByNationalId(@PathVariable String nationalId) {
+        return memberService.getMemberByNationalId(nationalId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Member createMember(@RequestBody Member member) {
+        return memberService.saveMember(member);
+    }
+}
