fcf63c6fe6f34ebcc0c1295eefaf9b3da4b60488
Daniel Fernández roldan <danielfernandezroldan2005@gmail.com>
2026-07-31 13:29:20 +0200

    Added a global exception handler.

---
+++ src/main/java/com/api/inventary_and_loans/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.api.inventary_and_loans.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Method that listen when a validation error is deployed. (@Valid)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        // We take only the name of the place where it fails and our personalized message.
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        // We return a clean list of errores with 400 Bad Request code.
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
