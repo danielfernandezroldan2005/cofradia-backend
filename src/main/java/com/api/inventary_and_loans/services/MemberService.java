562d53c4973fcebd82c94de1346d4d4d2ac4b2b7
Daniel Fernández roldan <danielfernandezroldan2005@gmail.com>
2026-07-31 13:33:03 +0200

    Added a page method in MemberService.

--- src/main/java/com/api/inventary_and_loans/services/MemberService.java
+++ src/main/java/com/api/inventary_and_loans/services/MemberService.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 @Service
 public class MemberService {
 
@@ -26,5 +30,11 @@ public class MemberService {
 
     public Member saveMember(Member member) {
         return memberRepository.save(member);
+    }
+
+    // Method for obtaining the member in a paginated form.
+    public Page<Member> getMembersPaginated(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return memberRepository.findAll(pageable);
     }
 }
