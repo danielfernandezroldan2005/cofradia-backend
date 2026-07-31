67a773b6bfe34a8b164dc2de3f70eafbc6e4b3df
Daniel Fernández roldan <danielfernandezroldan2005@gmail.com>
2026-07-31 12:45:33 +0200

    Added validation to the Member model.

--- src/main/java/com/api/inventary_and_loans/entities/Member.java
+++ src/main/java/com/api/inventary_and_loans/entities/Member.java
@@ -1,6 +1,9 @@
 package com.api.inventary_and_loans.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,10 +18,17 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
-    private Integer memberNumber;
-
+    @NotBlank(message = "Firstname is obligatory and it must not be blank.")
     private String firstName;
+
+    @NotBlank(message = "Lastname is obligatory and it must not be blank.")
     private String lastName;
+
+    @NotBlank(message = "DNI is obligatory.")
+    @Size(min = 9, max = 9, message = "DNI must have 9 characters.")
     private String nationalId; // For DNI
+
+    @Column(unique = true)
+    @NotNull(message = "Member number could not be null.")
+    private Integer memberNumber;
 }
